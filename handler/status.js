abdd8877ce99d4640abd5d7ec4a1316c486403d4
Lexxy <85262576+Snipedprox@users.noreply.github.com>
2022-01-18 20:00:10 -0300

    Create status.js

---
+++ handler/status.js
@@ -0,0 +1,7 @@
+module.exports = (bot) => {
+    bot.status({
+        text: ";help in $servercount guilds and listening to $allmemberscount users",
+        type: "PLAYING",
+        time: 12
+    })
+}
