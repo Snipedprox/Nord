5944d3a88a255899d9753240b476b78bec053487
Lexxy <85262576+Snipedprox@users.noreply.github.com>
2022-01-18 19:59:15 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+ const express = require('express');
+
+const server = express();
+
+server.all('/', (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+
+    res.write(
+        '<link href="https://fonts.googleapis.com/css?family=Roboto Condensed" rel="stylesheet"> <style> body {font-family: "Roboto Condensed";font-size: 22px;} <p>Hosting Active</p>'
+    );
+
+    res.end();
+});
+
+function keepAlive() {
+    server.listen(3000, () => {
+        console.log('Server is online!');
+    });
+}
+
+module.exports = keepAlive;
